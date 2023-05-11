ec793e369cd34cb9b094216f2a9c6444403329b5
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-05-11 20:47:06 -0300

    Implements OnChanges

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,14 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent implements OnInit, OnChanges {
   @Input() name: string = '';
 
+  constructor() {
+    console.log('constructor -' + this.name);
+  }
+
+  ngOnChanges(): void {
+    console.log('onChanges -' + this.name);
+  }
+
   ngOnInit(): void {
-    this.name = '' ? 'Other name' : this.name ;
+    console.log('onInit -' + this.name);
   }
 }
