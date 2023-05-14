89fc885e1eb0de6ae06dcf2193890ff6e89c4af8
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-05-13 21:17:05 -0300

    Implements DoCheck and others

---
+++ src/app/check-sample/check-sample.component.ts
@@ -0,0 +1,31 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck } from '@angular/core';
+
+@Component({
+  selector: 'app-check-sample',
+  templateUrl: './check-sample.component.html',
+  styleUrls: ['./check-sample.component.css']
+})
+export class CheckSampleComponent implements
+DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
+
+  ngDoCheck(): void {
+    console.log('DoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('AfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('AfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('AfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('AfterViewChecked');
+  }
+
+}
