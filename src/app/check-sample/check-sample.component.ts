cac4ffc556a4fcaadbfdd8194322f94886b43e4e
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-05-15 16:56:58 -0300

    check sample component lifecycle

--- src/app/check-sample/check-sample.component.ts
+++ src/app/check-sample/check-sample.component.ts
@@ -8,22 +8,38 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 export class CheckSampleComponent implements
 DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
 
+  quantidade: number = 0;
+
+  adicionar() {
+    this.quantidade += 1;
+  }
+
+  decrementar() {
+    this.quantidade = this.quantidade > 0 ? this.quantidade - 1 : 0;
+  }
+
+  // checked -> content - view
+
   ngDoCheck(): void {
     console.log('DoCheck');
   }
 
+  // quando o primeiro conteúdo é iniciado
   ngAfterContentInit(): void {
     console.log('AfterContentInit');
   }
 
-  ngAfterContentChecked(): void {
-    console.log('AfterContentChecked');
-  }
-
+  // depois da inicialização da view
   ngAfterViewInit(): void {
     console.log('AfterViewInit');
   }
 
+  // após alguma alteração, verifica o conteúdo
+  ngAfterContentChecked(): void {
+    console.log('AfterContentChecked');
+  }
+
+  // após alguma alteração, verifica a view
   ngAfterViewChecked(): void {
     console.log('AfterViewChecked');
   }
