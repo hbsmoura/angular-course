ffaa0909884e291f0d15af98b06d7a07924dd3a3
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-05-16 11:50:26 -0300

    ngFor

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+    produtos: string[] = []
 
+    constructor() {
+        this.produtos = ['mouse', 'teclado', 'cabo', 'fonte']
+    }
+
+    adicionar(): void {
+        this.produtos.push('novo')
+    }
+
+    remover(index: number): void {
+        this.produtos.splice(index, 1)
+    }
 }
